3c0720c8b1443fdcb93dea85fb60608fc34a0e47
fatanyrazis <you@example.com>
2019-12-16 00:09:26 +0700

    ganti kabeh

--- js.js
+++ js.js
@@ -1,50 +1,31 @@
 var header =$('h1')
 var tombol =$('#tombol')
 
-tombol.click(function()
-{
-    // header.text("Mantap cuy")
-    //pertama
-    //header.first().text("Mantap cuy")
-    //Kedua
-    //header.last().text("Mantap cuy")
+tombol.click(function(){    
+    
+    $('#id1').css("color", "white")
+    $('#id1').css("background-color", "blue")
+    $('#id1').html("Data Mahasiswa")
+    
+    console.log('clicked')
     $.getJSON("data.json",function(result){
         console.log(result)
         result.data.forEach((datum,key) => {
-            console.log(datum)
-
-            
-            // var nama="<td>"+datum.Nama+"</td>"
-            // var nim="<td>"+datum.Nim+"</td>"
-            // var email="<td>"+datum.email+"</td>"
-            // var kartu="<tr class='col-4 card'>"+nama+nim+email+"</div>"
-            // $('#list').append(kartu)
-            var id ="<td>"+(key+=1)+"</td>"
-            var nama="<td>"+datum.Nama+"</td>"
-            var nim="<td>"+datum.Nim+"</td>"
-            var email="<td>"+datum.email+"</td>"
-            var tr="<tr>"+id+nama+nim+email+"</tr>"
+                var id ="<td>"+(key+=1)+"</td>"
+                var nama="<td>"+datum.Nama+"</td>"
+                var nim="<td>"+datum.Nim+"</td>"
+                var email="<td>"+datum.email+"</td>"
+                var jumlah="<td>"+datum.jumlah+"</td>"
+                var matkul = ""
+                datum.matakuliah.forEach((item) => {
+                    matkul += "<li>"+item+"</li>"
+                });
+                console.log(matkul)
+                var matakuliah = "<td><ol>"+matkul+ "</ol></td>"
+            var tr="<tr>"+id+nama+nim+email+jumlah+matakuliah+"</tr>"  
             $('#tbody').append(tr)
-            
         });
 
     })
-  tombol.click(function()
-  {
-      
-  })  
+   
 })
-
-var tambah=$('#tambah')
-tambah.click(function(e)
-{
-    e.preventDefault()
-    var nama=$('#nama').val()
-    var nim=$('#nim').val()
-    var email=$('#email').val()
-    console.log(nama,nim,email)
-    $.getJSON('data.json',)    
-})
-
-header.val('halo dunia')
-console.log(header)
